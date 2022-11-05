c948e418a4dceb82e12dcabc05664bd9f11ffac8
marklen <marklen86@gmail.com>
2022-11-04 19:59:13 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/Dsa07QueuesApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsa07QueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsa07QueuesApplication.class, args);
+	}
+
+}
