f0b61272a585ce07f8df4bfe802a35d453f6c45a
marklen <marklen86@gmail.com>
2022-12-09 20:08:18 -0600

    Heap practice

--- dsa-09-Heap/src/main/java/HeapApp.java
+++ dsa-09-Heap/src/main/java/HeapApp.java
@@ -1,4 +1,4 @@
-public class HeapApp {
+ public class HeapApp {
     public static void main(String[] args) {
         MyHeap heap=new MyHeap(10);
         heap.insert(61);
@@ -7,5 +7,12 @@ public class HeapApp {
         heap.insert(45);
         heap.insert(81);
         heap.printHeap();
+        heap.remove();
+        heap.printHeap();
+        heap.insert(130);
+        heap.printHeap();
+        heap.remove();
+        heap.printHeap();
+        int[] numbers={1,2,5,6,8,21,0};
     }
 }
